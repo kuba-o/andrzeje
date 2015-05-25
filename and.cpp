c98e0561b35fad28b4cd8a30fc5280964a6dbeb8
kuba <kuba.ostrowicki@interia.pl>
2015-05-25 14:49:09 +0200

    cleared

--- and.cpp
+++ and.cpp
@@ -22,7 +22,6 @@ public:
 	int longest;
 	float distances[3];
 
-	
 	void calcDistances(){
 		for (int i = 1; i < 4; i++){
 			distances[i] = sqrt(pow((tabX[0]-tabX[i]), 2.0) + pow((tabY[0]-tabY[i]), 2.0));
@@ -67,36 +66,22 @@ int main(){
     		circle( pattern, center, 3, Scalar(0,255,0), -1, 8, 0 );
       		// circle outline
       		circle( pattern, center, radius, Scalar(0,0,255), 3, 8, 0 );
+      		/*
       		cout<<"radius: "<<radius<<endl;
       		cout<<"x: :"<<checkX<<endl;
       		cout<<"y :"<<checkY<<endl<<endl;
+      		*/
       		howMany++;
-      		cout<<"tu1"<<endl;
+
       		tab[indeks/4].tabX[indeks%4] = checkX;
       		tab[indeks/4].tabY[indeks%4] = checkY;
-      		cout<<"Tu INDEKS: "<<indeks/4<<endl;
-      		cout<<"tu2"<<endl;
-      		cout<<"indeks: "<<indeks<<endl;
+
       		indeks++;
   		}
 	}
 
-
-	cout<<"a tu?"<<endl;
 	tab[0].calcDistances();
 	
-	/*
-	int a, b;
-	cout<<"tab[0].tabx[0]: "<<tab[0].tabX[0];
-	a = tab[0].tabX[0]-tab[0].tabX[1];
-	b = tab[0].tabY[0] - tab[0].tabY[1];
-	cout<<"tab[0].tabx[1]: "<<tab[0].tabX[1];
-	cout<<endl<<"a: "<<tab[0].tabX[0]<<endl;
-	cout<<"b :"<<b<<endl;
-
-	cout<<endl<<sqrt(pow(a, 2.0) + pow(b, 2.0))<<endl;
-	cout<<endl<<endl<<a<<endl;
-	*/
 	for (int i =0; i<howMany/4; i++){
 
 	}
